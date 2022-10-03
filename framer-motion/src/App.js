f4320fc0b7e0792ca3b6ec755091d64af8af9911
jukangpark <skyxxx9339@naver.com>
2022-10-03 19:17:34 +0900

    Framer-motion 드래그 가능한 슬라이드 기능, 현재 버그 있음

--- framer-motion/src/App.js
+++ framer-motion/src/App.js
@@ -1,20 +1,28 @@
-import { motion } from "framer-motion";
-import { useState } from "react";
+import { motion, useSpring } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { useScroll } from "framer-motion";
+import { makeImagePath } from "./utils";
 
 const Btn = styled.button``;
 
-function Component() {
+function ScrollBar() {
   const { scrollYProgress } = useScroll();
+  // const scaleX = useSpring(scrollYProgress);
 
-  console.log(scrollYProgress);
+  useEffect(() => {
+    scrollYProgress.onChange((latest) => {
+      console.log("Page scroll:", latest);
+      // scrollXProgress : The scroll position between the defined offsets, as a value between 0 and 1
+    });
+  }, []);
 
   return (
     <motion.div
       style={{
         scaleX: scrollYProgress,
-        backgroundColor: "pink",
+        // scaleX,
+        backgroundColor: "red",
         height: "25px",
         position: "fixed",
         top: "0",
@@ -41,6 +49,29 @@ const Box = ({ x, y }) => (
 );
 
 const App = () => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+  const [movies, setMovies] = useState([]);
+
+  const API_KEY = "d3fbf3b20d42f2b6f9751d87fc9f03ed";
+  const BASE_PATH = "https://api.themoviedb.org/3";
+
+  useEffect(() => {
+    fetch(
+      `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=kr`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setMovies(data.results.slice(0, 10));
+      });
+  }, []);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
 
@@ -54,12 +85,38 @@ const App = () => {
 
   return (
     <div>
+      <ScrollBar />
       <div>
         <Btn onClick={plusX}>plusX</Btn>
         <Btn onClick={plusY}>plusY</Btn>
       </div>
       <Box x={x} y={y} />
-      <Component />
+      <motion.div
+        className="carousel"
+        ref={carousel}
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
+          {movies?.map((movie, index) => (
+            <motion.div className="item" key={index}>
+              <div
+                style={{
+                  minHeight: "40rem",
+                  minWidth: "30rem",
+                  backgroundImage: `url(${makeImagePath(
+                    movie.backdrop_path,
+                    "w500"
+                  )})`,
+                }}
+              />
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
       <section style={{ height: "100vh" }}>section</section>
       <section style={{ height: "100vh" }}>section</section>
       <section style={{ height: "100vh" }}>section</section>
