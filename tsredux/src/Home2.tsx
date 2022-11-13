d9c5e038d372e5ab3840035bb558d983e8ec1fb0
jukangpark <skyxxx9339@naver.com>
2022-11-13 20:21:58 +0900

    unknwon 타입

--- tsredux/src/Home2.tsx
+++ tsredux/src/Home2.tsx
@@ -6,6 +6,11 @@ const Home2 = () => {
   //   const state: any = useSelector((state) => state);
   //   const state: number | unknwon = useSelector((state) => state);
 
+  // TypeScript
+  // Any 와 같ㅣ 최상위 타입인 unknown 은 알 수 없는 타입을 의미합니다.
+  // Any 와 같이 Unknwon 에는 어떤 타입의 값도 할당할 수 있지만,
+  // Unknown 을 다른 타입에는 할당 할 수 없습니다.
+
   const dispatch = useDispatch();
 
   const handleMinus = () => {
